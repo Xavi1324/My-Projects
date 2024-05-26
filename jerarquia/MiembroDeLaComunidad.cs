11d1c0353f410191a35d8f180c61eadf421369ce
Xavi1324 <151955645+Xavi1324@users.noreply.github.com>
2024-05-25 22:43:30 -0400

    Diagrama herencias

---
+++ jerarquia/MiembroDeLaComunidad.cs
@@ -0,0 +1,8 @@
+public class MiembroDeLaComunidad {}
+public class Empleado  : MiembroDeLaComunidad {}
+public class Estudiante : MiembroDeLaComunidad {}
+public class exAlumno : MiembroDeLaComunidad {}
+public class Docente : Empleado {}
+public class Administractivo : Empleado {}
+public class Admministrador : Docente {}
+public class Maestro : Docente {}
