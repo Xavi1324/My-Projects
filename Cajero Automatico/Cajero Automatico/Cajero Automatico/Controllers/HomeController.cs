d71d370a2c1dd63c3f39830d6b7386d781999aee
Xavier Casilla <casillaxavier13@gmail.com>
2025-01-19 23:39:50 -0400

    Cajero

---
+++ Cajero Automatico/Cajero Automatico/Cajero Automatico/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Cajero_Automatico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cajero_Automatico.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Retiro()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cajero Automatico/Cajero Automatico/Cajero Automatico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cajero_Automatico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
