4425544bd0a3b4d4cb7f6fe510a1b4a242140f33
Xavier Casilla <casillaxavier13@gmail.com>
2025-03-08 13:42:00 -0500

    Some basic projects

--- StreamingAppWeb/StreamingAppWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StreamingAppWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
